bea6c3024b94a731d71d817606084988437f5eeb
ChaudryManan <manankhn81@gmail.com>
2025-05-05 15:08:50 +0500

    fix: index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-require("dotenv").config()
-const express=require ("express")
-const app=express()
-const port =4000
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
-app.get("/twitter",(req,res)=>{
-    res.send("abdul Manan ")
-})
-app.get("/login",(req,res)=>{
-    res.send("<h1>Follow Abdul Manan</h1> ")
-})
-app.listen(process.env.PORT,()=>{
-    console.log((`example app listening on ${port}`))
-})
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+export async function init() {
+  await connectDB();
+  return app;
+}
+
+// (Optional) This part only runs locally (not in serverless environments)
+if (process.env.NODE_ENV !== "production") {
+  init().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is listening on port ${process.env.PORT || 8000}`);
+    });
+
+    app.on("error", (error) => {
+      console.error("Server error:", error);
+      throw error;
+    });
+  }).catch((error) => {
+    console.error("There was an error in index.js:", error);
+  });
+}
