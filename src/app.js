86ac2d11821435843b8fd26d166bb4bd0b2b5484
ChaudryManan <manankhn81@gmail.com>
2025-05-09 14:39:06 +0500

    fixing cor

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ import cors from "cors";
 import userRouter from "./Routes/user.Routes.js";
 
 const app = express();
-
+app.use((req, res, next) => {
+  console.log("💧 Incoming:", req.method, req.url);
+  next();
+});
 
 app.use(cors({
   origin: [
